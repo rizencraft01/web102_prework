e92fd98c165313f8f93a65b2d8edfba4fb262b76
rizencraft01 <134184625+rizencraft01@users.noreply.github.com>
2026-05-25 22:03:03 -0400

    Add stats card with data

--- index.js
+++ index.js
@@ -71,19 +71,41 @@ addGamesToPage(GAMES_JSON)
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
+const initialValue = 0
+
+
+const totalContributions = GAMES_JSON.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.backers,
+    initialValue
+)
 
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
+
+contributionsCard.innerHTML = `${totalContributions.toLocaleString('en-US')}`
 
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
 
+const totalRaised = GAMES_JSON.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.pledged,
+    initialValue
+)
+
 // set inner HTML using template literal
 
+raisedCard.innerHTML = `$${totalRaised.toLocaleString('en-US')}`
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
+
+const totalGames = GAMES_JSON.reduce(
+    (accumulator, currentValue) => GAMES_JSON.indexOf(currentValue) + 1, 
+    initialValue
+)
+
+gamesCard.innerHTML = `${totalGames.toLocaleString('en-US')}`
 
 
 /*************************************************************************************
